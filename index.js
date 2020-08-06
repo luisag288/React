cbb309ef9325941d298557f5d73412ab0d4491a7
luisag288 <luisaaristizabal@franciscomiranda.edu.co>
2020-08-06 10:10:15 -0500

    Class1

---
+++ index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+ReactDOM.render(
+  <div>
+    <table>
+      <tr>
+        <td class="titulos">Nombres</td>
+        <td class="titulos">Apellidos</td>
+        <td class="titulos">Correo</td>
+        <td class="titulos">Celular</td>
+      </tr>
+
+      <tr>
+        <td>Ana Cristina </td>
+        <td>Pastrana Castillo</td>
+        <td>anapastrana@franciscomiranda.edu.co</td>
+        <td>3132154620</td>
+      </tr>
+
+      <tr>
+        <td>Jannelly</td>
+        <td>Garro Ruiz</td>
+        <td>jannelly140@gmail.com</td>
+        <td>3013912802</td>
+      </tr>
+
+      <tr>
+        <td>Luisa Fernanda</td>
+        <td>Aristizabal Giraldo</td>
+        <td>luisaaristizabal@franciscomiranda.edu.co</td>
+        <td>3126006877</td>
+      </tr>
+
+      <tr>
+        <td>Jorge Andrés</td>
+        <td>Cardona Giraldo</td>
+        <td>jorgecardona@franciscomiranda.edu.co</td>
+        <td>3116515496</td>
+      </tr>
+
+      <tr>
+        <td>Brayan Andrés</td>
+        <td>Ramirez Torres</td>
+        <td>brayanramirez@franciscomiranda.edu.co</td>
+        <td>3128024906</td>
+      </tr>
+
+
+    </table>
+
+  </div>,
+  document.getElementById('root')
+);
+
+
+
